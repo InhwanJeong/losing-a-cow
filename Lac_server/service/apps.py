055c48565221f1bd55e8a803c9a72adb6dccff2e
inhwanJeong <ghjklla007@naver.com>
2022-12-26 21:35:36 +0900

    feat: 백엔드 서비스 앱 생성

--- Lac_server/service/apps.py
+++ Lac_server/service/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjectConfig(AppConfig):
+class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'project'
+    name = 'service'
