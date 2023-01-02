dfecb7591e2055ae7c098eec16b41b5446517daa
inhwanJeong <ghjklla007@naver.com>
2023-01-02 23:42:04 +0900

    feat: 한국 소비자원 데이터 출력해 봄

--- Lac_datapipeline/scripts/main.py
+++ Lac_datapipeline/scripts/main.py
@@ -6,8 +6,35 @@ from data_preprocessing import DataPreprocessing
 
 class App:
     def __init__(self):
-        self.file_path = '../data'
+        self.file_path: str = '../data'
         self.files: list = os.listdir(self.file_path)
+
+    def start_app(self):
+        while True:
+            print("0. 종료\n1. 데이터 일부 조회")
+            input_data = int(input("파일이름 입력:"))
+            if not input_data:
+                break
+            self.__retrieve_data()
+
+    def __retrieve_data(self):
+        while True:
+            print("b 입력시 뒤로가기")
+            print("\n".join(self.files[:5]))
+            file = input("파일이름 입력:")
+            if file == 'b':
+                break
+            if file not in self.files:
+                continue
+            self.__read_csv(file)
+
+    def __read_csv(self, file):
+        with open(os.path.join(self.file_path, file), 'r', encoding='euc-kr') as f:
+            table = csv.reader(f)
+            for i, item in enumerate(table):
+                print(item)
+                if i == 10:
+                    break
 
     def get_data(self):
         pass
@@ -18,11 +45,4 @@ class App:
 
 if __name__ == '__main__':
     app = App()
-    with open(os.path.join('../data', app.files[1]), 'r', encoding='euc-kr') as f:
-        table = csv.reader(f)
-        for i, item in enumerate(table):
-            # if i == 0:
-            #     continue
-            print(item)
-            if i == 10:
-                break
+    app.start_app()
