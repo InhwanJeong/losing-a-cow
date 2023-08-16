7346670795ec3538b611366d00622d4708696e4f
inhwanJeong <ghjklla007@naver.com>
2023-08-16 15:21:08 +0900

    feat: 메인화면 방문페이지 개발완료

--- service/views.py
+++ service/views.py
@@ -1,13 +1,27 @@
+import datetime
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
 
 # Create your views here.
-from service.models import KCACustomerDangerousData
+from service.models import KCACustomerDangerousData, Inference
 
 
 def index(request):
-    return render(request, 'service/index.html')
+    queryset = Inference.objects.all()
+    age = request.GET.get('age')
+    result = dict()
+
+    return render(
+        request,
+        'service/index.html',
+        dict(
+            all_count=len(queryset),
+            today_count=len(queryset.filter(created_at__date=datetime.date.today())),
+            result=result,
+        ),
+    )
 
 
 def kca_list(request):
