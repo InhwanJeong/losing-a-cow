0e51d28c28bbe9cf9a94795745f51231f451d417
inhwanJeong <ghjklla007@naver.com>
2023-08-16 16:52:47 +0900

    feat: mvp 버전 프로덕트 개발완료

--- service/views.py
+++ service/views.py
@@ -2,16 +2,23 @@ import datetime
 
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Count
 
-
-# Create your views here.
 from service.models import KCACustomerDangerousData, Inference
 
 
 def index(request):
     queryset = Inference.objects.all()
     age = request.GET.get('age')
-    result = dict()
+
+    try:
+        inferences = _get_inference_result(int(age))
+    except BaseException:
+        return render(request, 'service/index.html', dict(
+            all_count=len(queryset),
+            today_count=len(queryset.filter(created_at__date=datetime.date.today())),
+            ),
+        )
 
     return render(
         request,
@@ -19,7 +26,8 @@ def index(request):
         dict(
             all_count=len(queryset),
             today_count=len(queryset.filter(created_at__date=datetime.date.today())),
-            result=result,
+            inferences=inferences,
+            age=age,
         ),
     )
 
@@ -40,3 +48,29 @@ def kca_list(request):
             page_numbers=posts.paginator.get_elided_page_range(page)
         )
     )
+
+
+def _get_inference_result(age: int):
+    queryset = KCACustomerDangerousData.objects.exclude(
+        affected_area='해당없음',
+    )
+    if age <= 5:
+        queryset = queryset.filter(age__lt=age)
+    else:
+        queryset = queryset.filter(
+            age__gte=age - 5,
+            age__lt=age + 5,
+        )
+
+    result = dict()
+    columns = ['injury_caused', 'injury_item', 'affected_area', 'affected_place']
+    for column in columns:
+        result[column] = list(
+            queryset.values(column).annotate(
+                count=Count(column)
+            ).order_by(
+                '-count'
+            )[:5]
+        )
+
+    return result
